9aa631a6dc1a6119e5cf350e60162036db3deea3
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-02-02 16:22:42 +0000

    replace html reports with allure

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,5 @@
 import os
-
+import allure
 import requests
 from conftest import product_api
 from utils.test_utils import generate_new_product_data
@@ -8,6 +8,10 @@ from utils.load_settings import settings
 api_url = settings['api-url']
 product_id = ''
 
+@allure.story("Test create product 0")
+@allure.title("Verify the create products API 0")
+@allure.description("verify the create product API response status code and data 0")
+@allure.severity("normal")
 def test_get_products():
     api_endpoint = api_url + "/products/"
     print(api_endpoint)
@@ -18,6 +22,10 @@ def test_get_products():
         assert 'id' in product
         assert product["id"] == product.get("id")
 
+@allure.story("Test get all products")
+@allure.title("Verify the get all products API")
+@allure.description("verify the get API response status code and data")
+@allure.severity("normal")
 def test_get_all_products(product_api):
     response = product_api.get_all_products()
 
@@ -26,6 +34,10 @@ def test_get_all_products(product_api):
         assert 'name' in product
         assert 'description' in product
 
+@allure.story("Test create product")
+@allure.title("Verify the create products API")
+@allure.description("verify the create product API response status code and data")
+@allure.severity("normal")
 def test_create_product(api_handler, product_api):
     global product_id
     product_data = generate_new_product_data(api_handler)
@@ -38,6 +50,10 @@ def test_create_product(api_handler, product_api):
     assert response['description'] == product_data.get("description")
     assert response['price'] == product_data.get("price")
 
+@allure.story("Test delete product")
+@allure.title("Verify the delete products API")
+@allure.description("verify the delete product API response status code and data")
+@allure.severity("normal")
 def test_delete_product_by_id(product_api):
     global product_id
 
@@ -45,5 +61,9 @@ def test_delete_product_by_id(product_api):
 
     assert response == 204
 
+@allure.story("Test get env vars")
+@allure.title("Verify the get env vars API")
+@allure.description("verify env vars are present")
+@allure.severity("normal")
 def test_env_vars():
     print(os.environ.get("EMAIL"))
